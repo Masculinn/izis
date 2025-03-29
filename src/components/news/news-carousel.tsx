8fb4d77ab2bb112b18f77b32b98e7527c41bab1b
Masculinn <hello@burakdev.com>
2025-03-29 18:07:36 +0100

    Fixed layout issues on news section

--- src/components/news/news-carousel.tsx
+++ src/components/news/news-carousel.tsx
@@ -6,6 +6,7 @@ import { Button as ButtonPrimitive } from "../ui";
 import { twJoin } from "tailwind-merge";
 import Link from "next/link";
 import { ArrowRight } from "lucide-react";
+import newsLib from "@/lib/newsLib";
 
 const timeoutAmount = 3000;
 
@@ -75,45 +76,39 @@ export const NewsCarousel = () => {
       onMouseLeave={handleInteractionEnd}
       onTouchStart={handleInteractionStart}
       onTouchEnd={handleInteractionEnd}
-      className="h-full w-full rounded-md aspect-square flex justify-between flex-col lg:col-span-2 lg:row-span-2 relative bg-transparent"
+      className="h-full w-full  aspect-square flex justify-between flex-col lg:col-span-2 lg:row-span-2 relative bg-transparent"
     >
-      <Carousel.Content
-        items={Array.from({ length: 10 }, (_, id) => ({ id: id + 1 }))}
-      >
-        {({ id }) => (
+      <Carousel.Content items={newsLib}>
+        {({
+          id,
+          content,
+          coordinates,
+          date,
+          images,
+          source,
+          subHeader,
+          title,
+          url,
+          type = "salvage-excavation-research",
+        }) => (
           <Carousel.Item id={id}>
-            <Card className="lg:h-11/12 h-5/6 w-full">
-              <Card.Header>
-                <Card.Title className="font-secondary flex flex-row gap-2 items-center">
-                  Title
-                </Card.Title>
-                <Card.Description className="tracking-tight">
-                  Aute excepteur veniam adipisicing quis minim veniam nostrud
-                  laboris adipisicing laboris.
-                </Card.Description>
-              </Card.Header>
-              <Card.Content className="flex aspect-square items-center justify-center ">
-                <span className="font-semibold text-4xl">{id}</span>
-              </Card.Content>
-              <Card.Footer className="">
-                <Link href={"/"}>
-                  <ButtonPrimitive
-                    intent="secondary"
-                    shape="circle"
-                    size="small"
-                  >
-                    <span>Learn More</span>
-                    <ArrowRight className="w-4 h-4 " />
-                  </ButtonPrimitive>
-                </Link>
-              </Card.Footer>{" "}
-            </Card>
+            <div
+              className="w-full rounded-lg border h-10/12 aspect-square relative justify-end items-center flex"
+              style={{
+                backgroundImage: `url(${images[0]})`,
+                backgroundSize: "cover",
+                backgroundPosition: "center",
+              }}
+            >
+              <div className="bg-gradient-to-t from-black/90 to-transparent h-full w-full absolute bottom-0 left-0 right-0 z-0" />
+              <div className="">asdasd</div>
+            </div>
           </Carousel.Item>
         )}
       </Carousel.Content>
       <div className="mt-4 flex items-center justify-between">
         <div className="flex gap-1 py-2 text-center text-muted-fg text-sm">
-          {Array.from({ length: 10 }).map((_, index) => (
+          {Array.from({ length: newsLib.length }).map((_, index) => (
             <Button
               className={twJoin(
                 "rounded-xl transition focus:outline-hidden cursor-pointer",
